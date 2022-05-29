bef3c4fc86e08d36cfa6f7a58c9da3768aa058f5
George Kadianakis <desnacked@riseup.net>
2022-05-29 10:53:07 +0200

    Simplify sharding testing logic

--- sharding.py
+++ sharding.py
@@ -10,16 +10,12 @@ from roots_of_unity import ROOTS_OF_UNITY
 
 MODULUS = b.curve_order
 
-DATA_BYTES_PER_BLOB = 31 * params.FIELD_ELEMENTS_PER_BLOB
-
-# XXX no error correction atm
 # Dimensions of our matrix
 N_MATRIX_ROWS = 4
 N_MATRIX_COLUMNS = 4
 
-# Each column is a sample
-SAMPLES_PER_BLOB = N_MATRIX_COLUMNS
 # Each cell of the matrix is a sample
+SAMPLES_PER_BLOB = N_MATRIX_COLUMNS
 N_SAMPLES_TOTAL = SAMPLES_PER_BLOB * N_MATRIX_ROWS
 
 
@@ -33,13 +29,8 @@ class Sample(object):
         assert len(data) == params.FIELD_ELEMENTS_PER_SAMPLE
         degree = params.FIELD_ELEMENTS_PER_SAMPLE
 
-        self.data_points = data
-
-        # Sanity check: they should be actual scalar field elements
-        for data_point in self.data_points:
-            assert data_point < MODULUS
-
         self.sample_index = sample_index
+        self.data_points = data
 
         row_polynomial = fft.fft(entire_row_data, MODULUS, ROOTS_OF_UNITY, inv=True)
         shifting_factor = ROOTS_OF_UNITY[degree*sample_index]
@@ -51,7 +42,6 @@ class Sample(object):
 
         shifting_factor = ROOTS_OF_UNITY[degree*self.sample_index]
         assert kzg.verify_kzg_multiproof(commitment, shifting_factor, self.data_points, self.multiproof)
-        print("verified multiproof: {:.3f}s".format(get_time_delta()))
 
 class Blob(object):
     """
@@ -59,14 +49,11 @@ class Blob(object):
 
     A blob has a bunch of samples and a commitment that corresponds to the polynomial
     """
-    def __init__(self, data):
+    def __init__(self, data_points):
         """Get a blob from a bunch of data bytes"""
+        assert len(data_points) == params.FIELD_ELEMENTS_PER_BLOB
 
-        # Caller should have given us the right amount of data
-        assert len(data) == DATA_BYTES_PER_BLOB
-
-        # Split the data into field elements
-        data_points = [int.from_bytes(data[i*31:(i+1)*31], byteorder='big') for i in range(params.FIELD_ELEMENTS_PER_BLOB)]
+        # Split data into samples
         self.samples = [Sample(i+1, data_points[i:i+16], data_points) for i in range(SAMPLES_PER_BLOB)]
 
         # Get commitment to polynomial
@@ -81,18 +68,16 @@ class BlobsMatrix(object):
         self.blobs = []
 
         for i in range(N_MATRIX_ROWS):
-            data = os.urandom(DATA_BYTES_PER_BLOB)
+            data = [random.randint(0, MODULUS) for i in range(params.FIELD_ELEMENTS_PER_BLOB)]
             blob = Blob(data)
             self.blobs.append(blob)
 
     def _get_sample(self, r):
         n_row = r // SAMPLES_PER_BLOB
         n_column = r % SAMPLES_PER_BLOB
-
         print("get_sample(): got r=%d -> %d row %d column" % (r, n_row, n_column))
 
         blob = self.blobs[n_row]
-
         return blob.samples[n_column], blob.commitment
 
     def get_random_sample(self):
